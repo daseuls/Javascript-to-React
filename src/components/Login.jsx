63bd0a172c9114a4dbbf23943ada5efd95223d3f
daseuls <dls2tmfs2@naver.com>
2021-09-06 00:15:27 +0900

    Feat : Login through username

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+
+export default function Login() {
+  const [loginState, setLoginState] = useState(true)
+  const savedUsername = localStorage.getItem("username")
+
+  useEffect(() => onPaintUsername(), [])
+
+  const onLoginSubmit = (e) => {
+    e.preventDefault()
+    const loginInputValue = e.target[0].value
+    localStorage.setItem("username", loginInputValue)
+    console.log("onLoginSubmit")
+    onPaintUsername()
+  }
+
+  const onPaintUsername = () => {
+    if (localStorage.getItem("username")) {
+      setLoginState(false)
+    }
+  }
+
+  return (
+    <>
+      <LoginForm loginState={!loginState} onSubmit={onLoginSubmit}>
+        <LoginInput type="text" placeholder="사용자 이름을 입력해주세요" />
+      </LoginForm>
+      <Greeting loginState={loginState}>반가워요, {savedUsername}님 !</Greeting>
+    </>
+  )
+}
+
+const LoginForm = styled.form`
+  display: ${(props) => (props.loginState ? "none" : "")};
+`
+
+const LoginInput = styled.input`
+  border: 1px solid gray;
+  border-radius: 3px;
+  padding: 10px;
+  width: 30%;
+  font-size: 12px;
+`
+const Greeting = styled.h2`
+  display: ${(props) => (props.loginState ? "none" : "")};
+`
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
+
+import { ThemeProvider } from "styled-components"
+import GlobalStyle from "./styles/GlobalStyle"
+import theme from "./styles/theme"
+
+import Main from "./pages/Main"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}></ThemeProvider>
+    <Main />
+  </>,
   document.getElementById("root")
 )
