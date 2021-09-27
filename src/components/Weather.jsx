8df5edc5e72d2bed50c8afb12d5fa8e8e38584c0
daseuls <dls2tmfs2@naver.com>
2021-09-27 16:20:48 +0900

    Feat: 날씨 정보 rendering

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,37 +1,63 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 
 const API_KEY = "af3cc8709eebf290ec8dd8a90bdb9d45"
 export default function Weather() {
+  const [weatherInfo, setWeatherInfo] = useState({
+    maxTemp: "",
+    minTemp: "",
+    weather: "",
+    name: "",
+  })
   useEffect(
     () => navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError),
     []
   )
 
-  // useEffect(() => {
-  //   fetch()
-  // }, [])
   const onGeoOk = (position) => {
     const lat = position.coords.latitude
     const lon = position.coords.longitude
-    console.log(position)
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=kr`
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        setWeatherInfo({
+          ...weatherInfo,
+          maxTemp: Math.floor(data.main.temp_max - 273),
+          minTemp: Math.floor(data.main.temp_min - 273),
+          weather: data.weather[0].main,
+          name: data.name,
+        })
       })
   }
-
+  console.log(weatherInfo)
   const onGeoError = () => {}
   return (
-    <div>
-      <WeatherTemperature></WeatherTemperature>
-      <WeatherDescription></WeatherDescription>
-    </div>
+    <WeatherContainer>
+      {weatherInfo.maxTemp && (
+        <WeatherTemperature>
+          {weatherInfo.maxTemp}°C / {weatherInfo.minTemp}°C
+        </WeatherTemperature>
+      )}
+      <WeatherDescription>{weatherInfo.weather}</WeatherDescription>
+    </WeatherContainer>
   )
 }
+const WeatherContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 3vh;
+  right: 3vw;
+`
 
-const WeatherTemperature = styled.p``
+const WeatherTemperature = styled.p`
+  font-weight: 700;
+  margin-bottom: 5px;
+`
 
-const WeatherDescription = styled.p``
+const WeatherDescription = styled.p`
+  font-weight: 700;
+`
